19d4daaa91e1a7d8d261d3c5130639984f81f010
melinatabor <melina.tabor@gmail.com>
2024-07-07 20:46:33 -0300

    add: BE

---
+++ DistribuidoraDelHaras/BE/BEUsuario.cs
@@ -0,0 +1,14 @@
+﻿
+namespace BE
+{
+    public class BEUsuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Activo { get; set; }
+    }
+}
