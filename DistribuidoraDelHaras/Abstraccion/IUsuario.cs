0f835529d915c3b0f92719e57a8d2446bf8d0248
melinatabor <melina.tabor@gmail.com>
2024-07-09 13:45:43 -0300

    add: Abstraccion

---
+++ DistribuidoraDelHaras/Abstraccion/IUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion
+{
+
+    public interface IUsuario
+    {
+        string Username { get; set; }
+        string Password { get; set; }
+        int Rol { get; set; }
+    }
+}
